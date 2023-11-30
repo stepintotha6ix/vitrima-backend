9035c5639401c4dcc7ac256ce94d248fcdceeb0b
Ivan Troshin <mxsdbsx@gmail.com>
2023-11-30 20:37:12 +1000

    commit

--- src/work/work.controller.ts
+++ src/work/work.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Query, Put} from '@nestjs/common';
-import { WorkService } from './work.service';
+
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Query, Put, HttpCode, UsePipes, ValidationPipe} from '@nestjs/common';
+
 import { CreateWorkDto } from './dto/create-work.dto';
 import { UpdateWorkDto } from './dto/update-work.dto';
 import { AuthCheckContractor } from 'src/auth/guards/authCheck.guard';
 import { IdValidationPipe } from 'src/pipes/id.validation.pipe';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { WorkService } from './work.service';
 
 @Controller('work')
 export class WorkController {
@@ -24,18 +27,32 @@ export class WorkController {
 		return this.workService.getAll(searchTerm)
 	}
 
+  @Get(':id')
+	
+	async get(@Param('id', IdValidationPipe) id: string) {
+		return this.workService.byId(id)
+	}
+
   @Get(':slug')
   findBySlug(@Param('slug') slug: string) {
     return this.workService.bySlug(slug);
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateJobDto: UpdateWorkDto) {
-    return this.workService.update(+id, updateJobDto);
-  }
+  @UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth()
+	async update(
+		@Param('id', IdValidationPipe) id: string,
+		@Body() dto: UpdateWorkDto
+	) {
+		return this.workService.update(id, dto)
+	}
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.workService.remove(+id);
-  }
+	@HttpCode(200)
+	@Auth()
+	async delete(@Param('id', IdValidationPipe) id: string, @Body() contractorId) {
+		return this.workService.delete(id, contractorId)
+	}
 }
--- src/work/work.service.ts
+++ src/work/work.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import {
+	ForbiddenException,
+	Injectable,
+	NotFoundException,
+} from '@nestjs/common'
 import { CreateWorkDto } from './dto/create-work.dto'
 import { UpdateWorkDto } from './dto/update-work.dto'
 import { Work } from './work.schema'
 import { InjectModel } from '@nestjs/mongoose'
-import mongoose, { Model, Types } from 'mongoose'
+import mongoose, { Model, ObjectId, Types } from 'mongoose'
 
 import { UserService } from 'src/user/user.service'
 import { User } from 'src/user/Schemas/user.schema'
@@ -13,7 +17,6 @@ export class WorkService {
 	constructor(
 		@InjectModel('Work') private readonly workModel: Model<Work>,
 		private readonly contractorService: UserService
-		
 	) {}
 
 	async createWork(workDto: CreateWorkDto) {
@@ -38,13 +41,13 @@ export class WorkService {
 		return work
 	}
 
-	async byTags(
-		tagIds: Types.ObjectId[]
-	): Promise<Work[]> {
+	async byTags(tagIds: Types.ObjectId[]): Promise<Work[]> {
 		return this.workModel.find({ tags: { $in: tagIds } }).exec()
 	}
 	async byContractor(contractorId: Types.ObjectId) {
-		const docs = await this.workModel.find({ contractorId: contractorId }).exec()
+		const docs = await this.workModel
+			.find({ contractorId: contractorId })
+			.exec()
 		if (!docs) throw new NotFoundException('Услуги не найдены')
 		return docs
 	}
@@ -61,26 +64,83 @@ export class WorkService {
 				],
 			}
 
-		return this.workModel.find(options)
+		return this.workModel
+			.find(options)
 			.select('-password -updatedAt -__v')
 			.sort({ createdAt: 'desc' })
-			
+
 			.exec()
 	}
 
-	async bySlug(slug: string) {
-		const doc = await this.workModel.findOne({ slug }).populate('contractors').exec()
+	async byId(_id: string) {
+		const doc = await this.workModel.findById(_id)
+		if (!doc) throw new NotFoundException('Работа не найдена')
 		return doc
 	}
-	async byGenres(tagIds: Types.ObjectId[]) {
-		return this.workModel.find({ tags: { $in: tagIds } }).exec()
+
+	async bySlug(slug: string) {
+		const doc = await this.workModel
+			.findOne({ slug })
+			.populate('contractors')
+			.exec()
+		return doc
 	}
 
-	update(id: number, updateWorkDto: UpdateWorkDto) {
-		return `This action updates a #${id} job`
+	async update(_id: string, dto: UpdateWorkDto) {
+		const workDoc = await this.workModel.findById(_id).exec()
+
+		if (!workDoc) {
+			throw new NotFoundException('Работа не найдена')
+		}
+
+		// Извлекаем contractorId из объекта
+		const contractorId = dto.contractorId
+
+		const workContractorIdString = workDoc.contractorId
+			? workDoc.contractorId.toString()
+			: null
+
+		if (workContractorIdString !== contractorId) {
+			throw new ForbiddenException(
+				'You do not have permission to delete this work'
+			)
+		}
+
+		const updateDoc = await this.workModel
+			.findByIdAndUpdate(_id, dto, {
+				new: true,
+			})
+			.exec()
+		if (!updateDoc) throw new NotFoundException('Работа не найдена')
+		return updateDoc
 	}
 
-	remove(id: number) {
-		return `This action removes a #${id} job`
+	async delete(id: string, contractorIdObj: { contractorId: string }) {
+		const workDoc = await this.workModel.findById(id).exec()
+
+		if (!workDoc) {
+			throw new NotFoundException('Work not found')
+		}
+
+		// Извлекаем contractorId из объекта
+		const contractorId = contractorIdObj.contractorId
+
+		const workContractorIdString = workDoc.contractorId
+			? workDoc.contractorId.toString()
+			: null
+
+		if (workContractorIdString !== contractorId) {
+			throw new ForbiddenException(
+				'You do not have permission to delete this work'
+			)
+		}
+
+		const deleteDoc = await this.workModel.findByIdAndRemove(id).exec()
+
+		if (!deleteDoc) {
+			throw new NotFoundException('Work not found')
+		}
+
+		return deleteDoc
 	}
 }
