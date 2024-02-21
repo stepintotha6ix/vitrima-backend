b83ed488fb7ae53c3a888a3b5db274d98a515761
Ivan Troshin <mxsdbsx@gmail.com>
2024-02-21 23:25:29 +1000

    change settings for nodemailer

--- src/auth/mail.service.ts
+++ src/auth/mail.service.ts
@@ -7,9 +7,10 @@ export class MailService {
 
     constructor() {
         this.transporter = nodemailer.createTransport({
-            host:   process.env.SMTP_HOST,
-            port:   process.env.SMTP_PORT,
+            host:  "smtp.gmail.com",
+            port:  587,
             secure: false,
+            connectionTimeout: 5000,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
