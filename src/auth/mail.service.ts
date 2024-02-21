097ef10c96578455386bf12ca79adcab7611a724
Ivan Troshin <mxsdbsx@gmail.com>
2024-02-21 23:30:38 +1000

    fix

--- src/auth/mail.service.ts
+++ src/auth/mail.service.ts
@@ -8,9 +8,9 @@ export class MailService {
     constructor() {
         this.transporter = nodemailer.createTransport({
             host:  "smtp.gmail.com",
-            port:  587,
-            secure: false,
-            connectionTimeout: 5000,
+            port:  465,
+            secure: true,
+            connectionTimeout: 10000,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
